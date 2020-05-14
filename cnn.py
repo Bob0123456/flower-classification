cadeb98fd7f945475fb505386d305120b9b9992b
chrigkou <34583795+chrigkou@users.noreply.github.com>
2020-05-14 21:43:49 +0300

    Update cnn.py
    added a Data Generator to produce more images

--- cnn.py
+++ cnn.py
@@ -2,6 +2,7 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 import open
 import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import plot_confusion_matrix
@@ -18,6 +19,21 @@ def print_confusion_matrix(probs, labels, classes):
 
 x_train, x_test, y_train, y_test = open.preprocess()
 
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=20,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range=0.2,  # Randomly zoom image
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+datagen.fit(x_train)
+
 # define the model
 model = Sequential()
 model.add(Conv2D(filters=32, kernel_size=(3, 3), padding="same", activation="relu", input_shape=(128, 128, 3)))
@@ -31,15 +47,18 @@ model.add(Dense(5, activation="softmax"))  # the output layer
 model.compile(optimizer=Adam(lr=0.001),
               loss='categorical_crossentropy',
               metrics=['accuracy'])
-model.fit(x_train, y_train,
+
+
+model.fit_generator(datagen.flow(x_train,y_train, batch_size=256),
           epochs=15,
-          batch_size=256)
+          validation_data = (x_test, y_test))
 
 
 pred = model.predict(x_test)
 score = model.evaluate(x_test, y_test, batch_size=256)
 print(score)
-
 model.summary()
+
+
 
 
